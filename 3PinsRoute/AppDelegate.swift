29eeb4de6916c1d21f409b3c8180f2ad85178764
Alex <kazemirov99@mail.ru>
2022-03-22 00:48:18 +0300

    First commit#	new file:   3PinsRoute/Assets.xcassets/AppIcon.appiconset/Contents.json

---
+++ 3PinsRoute/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  3PinsRoute
+//
+//  Created by Алексей Каземиров on 3/21/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+   
+
+}
+
